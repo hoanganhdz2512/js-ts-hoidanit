cb28ab279a36c37feff32ee62d8156c74e5eddd2
hoanganhdz25 <hoanganhcl2512@gmail.com>
2025-09-10 15:02:56 +0700

    Video 20 Toán tử trong js

---
+++ src/video 20/video20.js
@@ -0,0 +1,19 @@
+console.log("video 20");
+
+// let a = 5;
+// // const b = ++a; //=> tang ngay lap tuc
+// // const b = a++; 
+// const c = a--;
+
+// console.log("a=" + a);
+// // console.log("b=" + b);
+// console.log("c=" + c);
+
+const a = 5; // number
+const b = "5";// string
+
+//===
+console.log(a === b);
+
+//==
+// console.log(a == b); 
