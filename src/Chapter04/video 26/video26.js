a0841065cb0938e8522fa9b07e2f56d146c37541
hoanganhdz25 <hoanganhcl2512@gmail.com>
2025-09-10 16:38:36 +0700

    Chapter 4 Video 26 Câu lệnh điều kiện if/else/elseif

---
+++ src/Chapter04/video 26/video26.js
@@ -0,0 +1,21 @@
+console.log("video 26");
+
+const age = 15;
+
+if(age >= 18){
+    console.log("You are an adult");
+}else{
+    console.log("You are a minor");
+}
+
+// score => A, B, C, D, F
+ 
+const score = 10;
+if(score >= 9){
+    console.log("Giỏi");
+}else if(score >6 && score <9){
+    console.log("Khá");
+}else{
+    console.log("Trung bình");
+}
+
