5a620cd922845c38321933d8b46611c09c6cee00
hoanganhdz25 <hoanganhcl2512@gmail.com>
2025-09-10 16:03:35 +0700

    Video 22 Teemplate Strings

---
+++ src/video 22/video22.js
@@ -0,0 +1,11 @@
+console.log("video 22");
+
+const name = "hoidanit";
+const age = 25;
+
+const introduction = "My name is " + name + " and I am " + age + " years old.";
+console.log(introduction);
+
+const intro = `my name is ${name} 
+    and I am ${age} years old.`;
+console.log(intro);
