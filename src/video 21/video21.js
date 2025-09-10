5b6a9f798a314c2ad3ef074073cc80a9017d7265
hoanganhdz25 <hoanganhcl2512@gmail.com>
2025-09-10 15:49:40 +0700

    Video21 Câulệnh console trong js

---
+++ src/video 21/video21.js
@@ -0,0 +1,23 @@
+console.log("video 21");
+console.log("info");
+console.error("oops my mistake");
+console.warn("test warning");
+
+/* */ //=> cách comment nhiều dòng
+
+// const name = "Eric";
+// const age = 25;
+
+
+let name = "Eric";
+let age = 25;
+
+console.log("Name: " + name + ", Age: " + age); //cách viết cũ
+
+console.log("Name:", name, ",Age:", age); //cách viết mới
+
+console.log(name, age); //cách viết mới
+
+console.log("%cCảnh báo", "color: red; font-weight: bold;"); //cách viết mới
+
+
