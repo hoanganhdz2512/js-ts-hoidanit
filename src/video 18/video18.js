fca2df91f5b3ad16bf6e08bf311e983eb3eec4c2
hoanganhdz25 <hoanganhcl2512@gmail.com>
2025-09-10 14:34:54 +0700

    Video 18 Kiểu dữ liệu nguyên thuỷ

---
+++ src/video 18/video18.js
@@ -0,0 +1,31 @@
+
+// number type
+const myAge = 25;
+const myScore = 9.1;
+
+
+console.log("video 18");
+
+console.log(myAge);
+console.log(myScore);
+
+// string type
+const name = "hoidanit";
+const name1 = 'erc';
+const name2 = `javascript`;
+
+console.log(name);
+
+// boolean type
+const isTuideptrai = true;
+const isFail = false;
+
+console.log(isTuideptrai);
+
+// undefined
+let bla; 
+console.log(bla);
+
+// null
+let testNull = null;
+console.log(testNull);
