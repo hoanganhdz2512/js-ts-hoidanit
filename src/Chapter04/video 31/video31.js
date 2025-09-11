f199c88deb165af48b0765f0f07a1e59dafa7d92
hoanganhdz25 <hoanganhcl2512@gmail.com>
2025-09-11 15:39:28 +0700

    Video 31 Arrow function

---
+++ src/Chapter04/video 31/video31.js
@@ -0,0 +1,14 @@
+console.log("video 31");
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+
+
+//arrow function
+const sum = (a , b) => {
+    return a + b;
+}
+
+console.log(sum(5, 10));    
