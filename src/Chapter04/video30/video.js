53db4c913c6f3934906c5a7963f93c24f925216c
hoanganhdz25 <hoanganhcl2512@gmail.com>
2025-09-11 15:24:19 +0700

    Video 30 hàm(function)

---
+++ src/Chapter04/video30/video.js
@@ -0,0 +1,16 @@
+console.log("video 30");
+
+function greeting(){
+    console.log("Hello");
+}
+
+greeting();
+
+//sum a + b
+function sum(a, b){
+    return a + b;
+} 
+
+console.log(sum(5, 10)); 
+
+console.log("anh hoang", "dep trai");
