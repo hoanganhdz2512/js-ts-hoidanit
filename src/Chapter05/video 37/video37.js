2e4c0b849ecab5d6f33934ac287f763f4b13d64d
hoanganhdz25 <hoanganhcl2512@gmail.com>
2025-09-11 16:54:49 +0700

    Array

---
+++ src/Chapter05/video 37/video37.js
@@ -0,0 +1,10 @@
+console.log("video 37");
+
+// Example array of names
+const names = ["Alice", "Bob", "Charlie", "David", "Eve"];
+
+const other = ["Eric", 0, true, null, undefined, [1, 2, 3], "Frank"];
+
+console.log(names);
+
+console.log(other);
