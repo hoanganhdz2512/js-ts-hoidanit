569ad89b309c87376eaeb3d2eba2ea2c9d6202c6
hoanganhdz25 <hoanganhcl2512@gmail.com>
2025-09-12 14:47:00 +0700

    Video 42 Giới thiệu về đối tượng Object

---
+++ src/Chapter05/video 42/video42.js
@@ -0,0 +1,27 @@
+console.log("video 40");
+
+const score = [10, 8, 3, 4, 5];
+
+//key: value
+const sv1 = {
+  score: 10,
+  name: "Nguyen Van A",
+  address: {
+    city: "Hanoi",
+    country: "Vietnam",
+  },
+};
+
+const sv2 = {
+  score: 8,
+  name: "Eric",
+  address: {
+    city: "Hanoi",
+    country: "Vietnam",
+  },
+};
+
+const sinhvien = [sv1, sv2];
+
+console.log("score", score);
+console.log("sinhvien", sinhvien);
