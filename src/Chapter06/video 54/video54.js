a8550f552155ccd6e1bfa6f95bfa515805273024
hoanganhdz25 <hoanganhcl2512@gmail.com>
2025-09-15 16:08:39 +0700

    Video 54 alert

---
+++ src/Chapter06/video 54/video54.js
@@ -0,0 +1,8 @@
+console.log("video 54");
+
+const element = document.getElementById("login");
+
+element.addEventListener("click", () => {
+  console.log("you click login button");
+  alert("you click login button");
+});
